2a59ee4bea7bcf6131a12845d26f8d8b1112182c
e1093051 <e1093051@u.nus.edu>
2025-01-22 20:49:45 +0800

    Echo

--- src/main/java/Carolyn.java
+++ src/main/java/Carolyn.java
@@ -1,16 +1,20 @@
+import java.util.Scanner;
 public class Carolyn {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner scanner = new Scanner(System.in);
         String Greeting = "____________________________________________________________\n" +
                 " Hello! I'm Carolyn\n" +
                 " What can I do for you?\n";
         String Bye = "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
-        System.out.print(Greeting + Bye);
+        String line = "____________________________________________________________\n";
+        System.out.print(Greeting);
+        String s = scanner.nextLine();
+        while (!s.equals("bye")) {
+            System.out.println(line + s + "\n" + line);
+            s = scanner.nextLine();
+        }
+        System.out.print(Bye);
     }
 }
