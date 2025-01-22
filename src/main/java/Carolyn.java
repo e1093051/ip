a67ee47e42471a383ecbfeea2f3cc2bde433cb56
e1093051 <e1093051@u.nus.edu>
2025-01-22 20:33:36 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Carolyn.java
@@ -0,0 +1,16 @@
+public class Carolyn {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        String Greeting = "____________________________________________________________\n" +
+                " Hello! I'm Carolyn\n" +
+                " What can I do for you?\n";
+        String Bye = "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.print(Greeting + Bye);
+    }
+}
