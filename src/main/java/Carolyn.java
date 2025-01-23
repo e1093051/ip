d4710ee6fdf23906694f48f887bffb22e965a873
e1093051 <e1093051@u.nus.edu>
2025-01-23 17:33:23 +0800

    Add, List

--- src/main/java/Carolyn.java
+++ src/main/java/Carolyn.java
@@ -1,18 +1,32 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Carolyn {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String Greeting = "____________________________________________________________\n" +
-                " Hello! I'm Carolyn\n" +
-                " What can I do for you?\n";
-        String Bye = "____________________________________________________________\n" +
+        String indent = "    ";
+        String Greeting = "    ____________________________________________________________\n" +
+                indent + " Hello! I'm Carolyn\n" +
+                indent +" What can I do for you?\n" +
+                "    ____________________________________________________________\n";
+        String Bye = "    ____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        String line = "____________________________________________________________\n";
+                "    ____________________________________________________________\n";
+        String line = "    ____________________________________________________________\n";
         System.out.print(Greeting);
+        ArrayList<String> list = new ArrayList<String>();
         String s = scanner.nextLine();
         while (!s.equals("bye")) {
-            System.out.println(line + s + "\n" + line);
+            System.out.print(line);
+            if (s.equals("list")) {
+                //reference to https://www.geeksforgeeks.org/arraylist-foreach-method-in-java/
+                //reference to https://stackoverflow.com/questions/20961617/get-the-current-index-of-a-for-each-loop-iterating-an-arraylist
+                list.forEach(item -> System.out.println(indent + (list.indexOf(item) + 1) + ". " + item));
+            }
+            else {
+                list.add(s);
+                System.out.println(indent + "added: " + s);
+            }
+            System.out.print(line);
             s = scanner.nextLine();
         }
         System.out.print(Bye);
