175ea95aa4866a158876f419a04e6dd15d84af06
e1093051 <e1093051@u.nus.edu>
2025-01-23 19:28:48 +0800

    Mark as Done

--- src/main/java/Carolyn.java
+++ src/main/java/Carolyn.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
+
 public class Carolyn {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -13,17 +15,36 @@ public class Carolyn {
                 "    ____________________________________________________________\n";
         String line = "    ____________________________________________________________\n";
         System.out.print(Greeting);
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Task> list = new ArrayList<Task>();
         String s = scanner.nextLine();
         while (!s.equals("bye")) {
             System.out.print(line);
             if (s.equals("list")) {
                 //reference to https://www.geeksforgeeks.org/arraylist-foreach-method-in-java/
                 //reference to https://stackoverflow.com/questions/20961617/get-the-current-index-of-a-for-each-loop-iterating-an-arraylist
-                list.forEach(item -> System.out.println(indent + (list.indexOf(item) + 1) + ". " + item));
+                list.forEach(item -> System.out.println(indent + (list.indexOf(item) + 1) + ".[" + item.getStatusIcon() + "]" + item.getDescription()));
+            }
+            //https://www.tutorialspoint.com/java/java_string_matches.htm
+            else if (s.matches("mark \\d")) {
+                String[] array = s.split(" ");
+                //https://www.geeksforgeeks.org/how-to-convert-string-to-int-in-java/
+                int index = Integer.valueOf(array[1]);
+                Task t = list.get(index);
+                t.mark(true);
+                System.out.println(indent + " Nice! I've marked this task as done:");
+                System.out.println(indent + "   [X] " + t.getDescription());
+            }
+            else if (s.matches("unmark \\d")) {
+                String[] array = s.split(" ");
+                //https://www.geeksforgeeks.org/how-to-convert-string-to-int-in-java/
+                int index = Integer.valueOf(array[1]);
+                Task t = list.get(index);
+                t.mark(false);
+                System.out.println(indent + " OK, I've marked this task as not done yet:");
+                System.out.println(indent + "   [ ] " + t.getDescription());
             }
             else {
-                list.add(s);
+                list.add(new Task(s));
                 System.out.println(indent + "added: " + s);
             }
             System.out.print(line);
