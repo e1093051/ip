54bdc458291235d8e55ebd4e3dea2a6d214760a9
e1093051 <e1093051@u.nus.edu>
2025-02-14 12:35:33 +0800

    Set up serial version

--- src/main/java/carolyn/TaskList.java
+++ src/main/java/carolyn/TaskList.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class TaskList implements Serializable {
+    private static final long serialVersionUID = 1L;
     protected ArrayList<Task> tasks;
 
     public TaskList() {
